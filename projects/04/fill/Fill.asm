7fec1d52cf038fcffcaa93b1ac98b0d0e2b20e80
Zhixiang <lzx.inf@gmail.com>
2021-05-13 21:20:23 +0800

    Implement projects-04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,56 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(MAIN)
+    @KBD
+    D=M
+    @BLACK
+    D, JGT
+    @WHITE
+    D, JEQ
+
+(BLACK)
+    @R0
+    M=-1
+    @DRAW
+    0;JMP
+
+(WHITE)
+    @R0
+    M=0
+    @DRAW
+    0;JMP
+
+(DRAW)
+    // i = @SCREEN + (8192 - 1)
+    @8191
+    D=A
+    @SCREEN
+    D=A+D
+    @i
+    M=D
+
+    (LOOP)
+        // get color
+        @R0
+        D=M
+
+        // set color
+        @i
+        A=M
+        M=D
+
+        // i = i - 1
+        @i
+        M=M-1
+
+        // i >= @SCREEN?
+        @SCREEN
+        D=A
+        @i
+        D=M-D
+        @MAIN
+        D;JLT
+
+    @LOOP
+    0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,33 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// result
+    @R2
+    M=0
+
+// store counter to R3
+    @R1
+    D=M
+    @R3
+    M=D
+
+(LOOP)
+// current counter
+    @R3
+    D=M
+    @END
+    D;JLE
+// R2+=R0
+    @R0
+    D=M
+    @R2
+    M=D+M
+// R3=R3-1
+    @R3
+    M=M-1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
