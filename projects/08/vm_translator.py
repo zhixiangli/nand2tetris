9fa337a02e0145850ad54fe1681e1ff3f9afc51e
Zhixiang <lzx.inf@gmail.com>
2021-05-23 13:04:01 +0800

    Reformat vm_translator.py for project-08

--- projects/08/vm_translator.py
+++ projects/08/vm_translator.py
@@ -13,15 +13,13 @@ class VMTranslator:
             "argument": "ARG",
             "that": "THAT",
             "this": "THIS",
-            "local": "LCL"
+            "local": "LCL",
         }
         self.__filename = filename
 
     def translate(self, lines: list[str]) -> list[str]:
         return self.__handle_vm_code(
-            self.__handle_spaces(
-                self.__handle_comments(lines)
-            )
+            self.__handle_spaces(self.__handle_comments(lines))
         )
 
     def __handle_vm_code(self, lines: list[str]) -> list[str]:
@@ -30,24 +28,32 @@ class VMTranslator:
             results.append(f"// {line}")
             tokens: list[str] = line.split()
             command: str = tokens[0]
-            if command == 'push':
+            if command == "push":
                 segment: Optional[str] = tokens[1] if len(tokens) > 1 else None
                 index: Optional[int] = int(tokens[2]) if len(tokens) > 2 else None
                 results.extend(self.__translate_push(segment, index))
-            elif command == 'pop':
+            elif command == "pop":
                 segment: Optional[str] = tokens[1] if len(tokens) > 1 else None
                 index: Optional[int] = int(tokens[2]) if len(tokens) > 2 else None
                 results.extend(self.__translate_pop(segment, index))
-            elif command == 'label':
+            elif command == "label":
                 results.extend(self.__translate_label(label=tokens[1]))
-            elif command == 'goto':
+            elif command == "goto":
                 results.extend(self.__translate_goto(label=tokens[1]))
-            elif command == 'if-goto':
+            elif command == "if-goto":
                 results.extend(self.__translate_if_goto(label=tokens[1]))
             elif command == "function":
-                results.extend(self.__translate_function(function_name=tokens[1], n_vars=int(tokens[2])))
+                results.extend(
+                    self.__translate_function(
+                        function_name=tokens[1], n_vars=int(tokens[2])
+                    )
+                )
             elif command == "call":
-                results.extend(self.__translate_call(function_name=tokens[1], n_args=int(tokens[2])))
+                results.extend(
+                    self.__translate_call(
+                        function_name=tokens[1], n_args=int(tokens[2])
+                    )
+                )
             elif command == "return":
                 results.extend(self.__translate_return())
             else:
@@ -105,7 +111,7 @@ class VMTranslator:
                 f"A=M{'-' if offset < 0 else '+'}D",
                 "D=M",
                 f"@{target}",
-                "M=D"
+                "M=D",
             ]
 
         return [
@@ -182,21 +188,18 @@ class VMTranslator:
 
     def __translate_push(self, segment: str, index: int) -> list[str]:
         commands: list[str] = self.__select_address(segment, index)
-        if segment == 'constant':
-            commands.extend([
-                f"@{index}",
-                "D=A"
-            ])
+        if segment == "constant":
+            commands.extend([f"@{index}", "D=A"])
         else:
-            commands.extend([
-                "D=M"
-            ])
-        commands.extend([
-            "@SP",
-            "M=M+1",
-            "A=M-1",
-            "M=D",
-        ])
+            commands.extend(["D=M"])
+        commands.extend(
+            [
+                "@SP",
+                "M=M+1",
+                "A=M-1",
+                "M=D",
+            ]
+        )
         return commands
 
     def __translate_pop(self, segment: str, index: int) -> list[str]:
@@ -221,7 +224,7 @@ class VMTranslator:
                 self.__translate_pop("temp", 6),
                 self.__select_address("temp", 6),
                 f"M={operator}M",
-                self.__translate_push("temp", 6)
+                self.__translate_push("temp", 6),
             ]
 
         def binary(operator: str) -> list[str]:
@@ -237,12 +240,15 @@ class VMTranslator:
                 "@R13",
                 "A=M",
                 f"M=D{operator}M",
-                self.__translate_push("temp", 7)
+                self.__translate_push("temp", 7),
             ]
 
         def compare(jump: str):
-            true_label, false_label, main_label = f"true_label_{self.__label_id}", f"false_label_{self.__label_id}", \
-                                                  f"main_label_{self.__label_id}"
+            true_label, false_label, main_label = (
+                f"true_label_{self.__label_id}",
+                f"false_label_{self.__label_id}",
+                f"main_label_{self.__label_id}",
+            )
             self.__label_id += 1
             return [
                 self.__translate_pop("temp", 7),
@@ -252,7 +258,6 @@ class VMTranslator:
                 "@R13",
                 "M=D",
                 self.__select_address("temp", 6),
-
                 "D=M",
                 "@R13",
                 "A=M",
@@ -260,38 +265,35 @@ class VMTranslator:
                 f"@{true_label}",
                 f"D;{jump}",
                 self.__translate_goto(false_label),
-
                 f"({true_label})",
                 "D=-1",
                 self.__translate_goto(main_label),
-
                 f"({false_label})",
                 "D=0",
                 self.__translate_goto(main_label),
-
                 f"({main_label})",
                 self.__select_address("temp", 7),
                 "M=D",
                 self.__translate_push("temp", 7),
             ]
 
-        if command == 'add':
+        if command == "add":
             return binary("+")
-        elif command == 'sub':
+        elif command == "sub":
             return binary("-")
-        elif command == 'neg':
+        elif command == "neg":
             return unary("-")
-        elif command == 'eq':
+        elif command == "eq":
             return compare("JEQ")
-        elif command == 'gt':
+        elif command == "gt":
             return compare("JGT")
-        elif command == 'lt':
+        elif command == "lt":
             return compare("JLT")
-        elif command == 'and':
+        elif command == "and":
             return binary("&")
-        elif command == 'or':
+        elif command == "or":
             return binary("|")
-        elif command == 'not':
+        elif command == "not":
             return unary("!")
         else:
             raise NotImplementedError(f"Unknown command {command}")
@@ -300,21 +302,31 @@ class VMTranslator:
         return [line.strip() for line in lines if line.strip()]
 
     def __handle_comments(self, lines: list[str]) -> list[str]:
-        return [line.split('//')[0] for line in lines]
+        return [line.split("//")[0] for line in lines]
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Translate VM Code to Hack Assembly Code')
-    parser.add_argument('vm', help='filepath or directory of VM code')
-    parser.add_argument('--booting', default=False, action=argparse.BooleanOptionalAction,
-                        help='call Sys.init if the function exist')
-    parser.add_argument("--sp", type=int, default=None,
-                        help="set the value to SP when booting is True")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Translate VM Code to Hack Assembly Code"
+    )
+    parser.add_argument("vm", help="filepath or directory of VM code")
+    parser.add_argument(
+        "--booting",
+        default=False,
+        action=argparse.BooleanOptionalAction,
+        help="call Sys.init if the function exist",
+    )
+    parser.add_argument(
+        "--sp", type=int, default=None, help="set the value to SP when booting is True"
+    )
     args = parser.parse_args()
 
     input_path: str = os.path.abspath(args.vm)
-    vm_files: list[str] = glob.glob(f"{input_path}/*.vm") if os.path.isdir(input_path) \
+    vm_files: list[str] = (
+        glob.glob(f"{input_path}/*.vm")
+        if os.path.isdir(input_path)
         else ([input_path] if input_path.endswith(".vm") else [])
+    )
     assert len(vm_files) > 0, f"No *.vm file is found from {input_path}"
 
     asm_code: list[str] = []
@@ -324,20 +336,31 @@ if __name__ == '__main__':
         with open(vm_file, "r") as input_file:
             code: list[str] = input_file.read().splitlines()
             asm_code.extend(translator.translate(code))
-    output_path = (input_path + f"/{os.path.basename(input_path)}" if os.path.isdir(input_path)
-                   else input_path.rstrip(".vm")) + ".asm"
+    output_path = (
+        input_path + f"/{os.path.basename(input_path)}"
+        if os.path.isdir(input_path)
+        else input_path.rstrip(".vm")
+    ) + ".asm"
 
     asm_code = list(deepflatten(asm_code, ignore=str))
     if args.booting and "(Sys.init)" in asm_code:
         # inject the bootstrap code
-        asm_code = ([
-                        f"@{args.sp}",
-                        "D=A",
-                        "@SP",
-                        "M=D",
-                    ] if args.sp is not None else []) + [
-                       "@Sys.init",
-                       "0;JMP",
-                   ] + asm_code
+        asm_code = (
+            (
+                [
+                    f"@{args.sp}",
+                    "D=A",
+                    "@SP",
+                    "M=D",
+                ]
+                if args.sp is not None
+                else []
+            )
+            + [
+                "@Sys.init",
+                "0;JMP",
+            ]
+            + asm_code
+        )
     with open(output_path, "w") as output_file:
         output_file.write("\n".join(asm_code))
